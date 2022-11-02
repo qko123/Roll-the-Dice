05e9ab4a9451979f0cfdaff98d2d99d1c83fff83
Quinn Kozak <quinn.kozak@icloud.com>
2022-11-02 09:23:56 -0500

    Initial Commit

---
+++ Roll the Dice/Roll_the_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_DiceApp.swift
+//  Roll the Dice
+//
+//  Created by Quinn Kozak on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
